4df869edd2c0d0d64b0fd2cf17d44d5a9d0d7f1f
Denis Rafi <62852838+DenisError404@users.noreply.github.com>
2020-05-02 16:44:14 +0100

    Add files via upload

---
+++ C# How to Draw a Lock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("By Denis Rafi");
+            Console.WriteLine("-----------------");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}{1}{0}",
+                 new string('_', n),
+                 new string('^', n));
+
+            Console.WriteLine("{0}{1}{2}{1}{0}",
+                new string('_', n - 2),
+                new string('^', 3),
+                new string('_', n - 2));
+
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                Console.WriteLine("{0}{1}{2}{3}{4}{3}{2}{1}{0}",
+                    new string('_', ((n - 4) - 2 * i)),
+                    new string('^', 2),
+                    new string('_', 2 + 2 * i),
+                    new string('^', 1),
+                    new string('_', n - 2));
+            }
+
+            for (int i = 0; i < n / 2 - 1; i++)
+            {
+                Console.WriteLine("{0}{1}{2}{3}{4}{3}{2}{1}{0}",
+                    new string('_', (1 + (2 * i))),
+                    new string('^', 2),
+                    new string('_', (n - 3) - 2 * i),
+                    new string('^', 1),
+                    new string('_', n - 2));
+            }
+
+            Console.WriteLine("{0}{1}{2}{1}{0}",
+                    new string('_', n - 2),
+                    new string('^', 3),
+                    new string('_', n - 2));
+
+            Console.WriteLine("{0}{1}{0}",
+                     new string('_', n),
+                     new string('^', n));
+            Console.ReadKey();
+        }
+        
+    }
+}
